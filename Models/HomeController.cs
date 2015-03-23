6040491a2a559de2362d75aa30f9b47e08057dda
quannt <simpleplan.vn@gmail.com>
2015-03-23 11:25:18 +0800

    initial commit

---
+++ Models/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tesseract_aspnet.Models
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        //
+        // GET: /Home/Details/5
+
+       
+    }
+}
